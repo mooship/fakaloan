3ce977e9cd282e9547368e49c3b11669ca4680ac
mooship <timothybrits@icloud.com>
2025-04-27 16:25:47 +0200

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -18,40 +18,5 @@ export default defineConfig({
         drop_console: true,
       },
     },
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Group Firebase and Vuefire together
-          if (id.includes('firebase') || id.includes('vuefire')) {
-            return 'vendor-firebase';
-          }
-          // Group core Vue libraries (Vue, Router, Pinia)
-          if (
-            id.includes('@vue') ||
-            id.includes('vue-router') ||
-            id.includes('pinia')
-          ) {
-            return 'vendor-vue';
-          }
-          // Group form validation libraries
-          if (id.includes('vee-validate') || id.includes('yup')) {
-            return 'vendor-forms';
-          }
-          // Separate vue-i18n
-          if (id.includes('vue-i18n')) {
-            return 'vendor-i18n';
-          }
-          // Separate vue-toastification
-          if (id.includes('vue-toastification')) {
-            return 'vendor-toast';
-          }
-          // Catch-all for other node_modules dependencies
-          // This ensures other smaller dependencies are also grouped
-          if (id.includes('node_modules')) {
-            return 'vendor-others';
-          }
-        },
-      },
-    },
   },
 });
