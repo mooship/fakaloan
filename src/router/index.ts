f18de47f1df39c4333d8a6e0299012186a79c428
mooship <timothybrits@icloud.com>
2025-04-28 10:36:25 +0200

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -34,6 +34,12 @@ const router = createRouter({
       component: () => import('../views/ForgotPasswordView.vue'),
       meta: { requiresGuest: true },
     },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import('../views/ProfileView.vue'),
+      meta: { requiresAuth: true },
+    },
   ],
 });
 
