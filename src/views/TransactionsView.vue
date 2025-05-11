6eac031892ce44be6cbc16796ced93719ef02ef7
mooship <timothybrits@icloud.com>
2025-05-11 10:27:02 +0200

    Swap colours

--- src/views/TransactionsView.vue
+++ src/views/TransactionsView.vue
@@ -55,8 +55,8 @@
                 class="ml-2 rounded px-2 py-0.5 text-xs"
                 :class="
                   tx.type === TransactionTypeEnum.Credit
-                    ? 'bg-green-100 text-green-700'
-                    : 'bg-blue-100 text-blue-700'
+                    ? 'bg-red-100 text-red-700'
+                    : 'bg-green-100 text-green-700'
                 "
               >
                 {{
